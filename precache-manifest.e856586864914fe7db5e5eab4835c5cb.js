09ee6bbc8df2d181db89955e84e06248d54458b6
paulfredmartin <paulmartin137@gmail.com>
2020-09-15 13:04:22 +0100

    Updates

---
+++ precache-manifest.e856586864914fe7db5e5eab4835c5cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04d85fe63a86c41a36c3ff0e572f064",
+    "url": "/my-app1/index.html"
+  },
+  {
+    "revision": "7d18b6b275cc598e876f",
+    "url": "/my-app1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/my-app1/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app1/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d18b6b275cc598e876f",
+    "url": "/my-app1/static/js/main.fe5674c7.chunk.js"
+  },
+  {
+    "revision": "66108b8c55bd034bc473",
+    "url": "/my-app1/static/js/runtime-main.647777ae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app1/static/media/logo.5d5d9eef.svg"
+  }
+]);
